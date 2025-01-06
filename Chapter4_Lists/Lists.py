80bb27a1b84ccf339d0abc17c1588115cf7263ec
kshitij-RGB <kshitijsaxena2004@gmail.com>
2025-01-06 12:28:41 +0530

    List and tuples

---
+++ Chapter4_Lists/Lists.py
@@ -0,0 +1,8 @@
+l = [] #Empty list
+print(type(l))
+# items = ["Kshitij", 9893244888, "Saxena", 5.10, True]
+# print(type(items[3]))
+
+# print(type(items))
+
+# #Unlike strings, Lists are mutable.
---
+++ Chapter4_Lists/ListsMethods.py
@@ -0,0 +1,16 @@
+profile = ["Kshitij", 21, True, 5.10, "IT"]
+profile.append("Saxena")
+print(profile)
+
+nums = [1,6,75,2,55,11]
+nums.reverse()
+print(nums)
+nums.sort()
+print(nums)
+nums.insert(3,10)
+print(nums)
+print(nums.pop(4)) #To delete on basis of index
+print(nums)
+nums.remove(75) #To delete in basis of value
+print(nums)
+
